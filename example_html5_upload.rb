fa30060751d36106947c6d55ff6bf8623bd716b0
Pablo Brasero <pablo@new-bamboo.co.uk>
2010-07-29 15:17:52 +0100

    Initial version

---
+++ example_html5_upload.rb
@@ -0,0 +1,87 @@
+require 'rubygems'
+require 'sinatra'
+require 'yaml'
+
+get '/' do
+  HTML_CODE
+end
+
+post '/' do
+  params[:file].to_yaml
+end
+
+
+#
+# Javascript code
+#
+
+JS_CODE =<<-JS
+document.getElementById('the-file').onchange = function () {
+  var fileInput = document.getElementById('the-file');
+  var file = fileInput.files[0];
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/', true);
+  xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
+  xhr.upload.addEventListener('progress', onprogressHandler, false);
+  xhr.upload.addEventListener('load', onloadHandler, false);
+  xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
+  xhr.setRequestHeader("Content-Type", "application/octet-stream");
+  console.log('Sending ', file)
+  xhr.send(file); // Simple!
+
+  function onloadstartHandler(evt) {
+      $('#upload-status').html('Upload started!');
+  }
+
+  function onloadHandler(evt) {
+      $('#upload-status').html('Upload successful!');
+  }
+
+  function onprogressHandler(evt) {
+      var percent = event.loaded/event.total*100;
+      $('#progress').html('Progress: ' + percent + '%');
+  }
+  
+  function onreadystatechangeHandler(evt) {
+      var status = null;
+
+      try {
+          status = evt.target.status;
+      }
+      catch(e) {
+          return;
+      }
+
+      if (status == '200' && evt.target.responseText) {
+        $('#result').html(evt.target.responseText);
+      }
+  }
+}
+JS
+
+
+#
+# HTML code
+#
+
+HTML_CODE =<<-HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>Ajax upload form</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+</head>
+<body>
+
+<input id="the-file" type=file name=file />
+<script>
+#{JS_CODE}
+</script>
+<p id="upload-status"></p>
+<p id="progress"></p>
+<pre id="result"></pre>
+</body>
+</html>
+HTML
