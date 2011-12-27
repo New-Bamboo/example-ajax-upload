3264120029db09ac4dea5ce7275cd7d2794ba8de
Pablo Brasero Moreno <pablobm@sunspear.(none)>
2011-12-27 14:52:28 +0100

    Example showing both ways to use FormData

--- example_html5_upload.rb
+++ example_html5_upload.rb
@@ -5,8 +5,17 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+post '/' do
+  our_file = params.delete('our-file')
+  "Received form fields:\n\n" + pretty_str(params) +
+    "\nAnd uploaded file:\n\n" + file_info(our_file)
+end
+
 post '/upload' do
-  file = params['our-file']
+  file_info params['our-file']
+end
+
+def file_info(file)
   details = {
     :filename => file[:filename],
     :type => file[:type],
@@ -15,5 +24,9 @@ post '/upload' do
     :tempfile_path => file[:tempfile].path,
     :tempfile_size => file[:tempfile].size,
   }
-  ''.tap{|output| PP.pp(details, output) }
+  pretty_str details
+end
+
+def pretty_str(obj)
+  ''.tap{|output| PP.pp(obj, output) }
 end
