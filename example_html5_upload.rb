4fa163fd25a4d923c576a025c01b6b0314e9ef99
Pablo Brasero Moreno <pablobm@sunspear.(none)>
2011-12-27 14:03:30 +0100

    Only the client code is relevant, so put it on its own

--- example_html5_upload.rb
+++ example_html5_upload.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'pp'
 
 get '/' do
-  HTML_CODE
+  send_file File.join(settings.public_folder, 'index.html')
 end
 
 post '/upload' do
@@ -17,89 +17,3 @@ post '/upload' do
   }
   ''.tap{|output| PP.pp(details, output) }
 end
-
-
-#
-# Javascript code
-#
-
-JS_CODE =<<-JS
-var uploadBtn = document.getElementById('upload-button-id');
-uploadBtn.onclick = function (evt) {
-  var fileInput = document.getElementById('file-id');
-  var file = fileInput.files[0];
-  var formData = new FormData();
-  var action = '/upload';
-  formData.append('our-file', file);
-
-  var xhr = new XMLHttpRequest();
-  xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
-  xhr.upload.addEventListener('progress', onprogressHandler, false);
-  xhr.upload.addEventListener('load', onloadHandler, false);
-  xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
-  xhr.open('POST', action, true);
-  xhr.send(formData); // Simple!
-
-  return false;
-
-  function onloadstartHandler(evt) {
-    var div = document.getElementById('upload-status');
-    div.innerHTML = 'Upload started!';
-  }
-
-  function onloadHandler(evt) {
-    var div = document.getElementById('upload-status');
-    div.innerHTML = 'Upload successful!';
-  }
-
-  function onprogressHandler(evt) {
-    var div = document.getElementById('progress');
-    var percent = evt.loaded/evt.total*100;
-    div.innerHTML = 'Progress: ' + percent + '%';
-  }
-  
-  function onreadystatechangeHandler(evt) {
-      var status = null;
-
-      try {
-          status = evt.target.status;
-      }
-      catch(e) {
-          return;
-      }
-
-      if (status == '200' && evt.target.responseText) {
-        var result = document.getElementById('result');
-        result.innerHTML = '<p>The server saw it as:</p><pre>' + evt.target.responseText + '</pre>';
-      }
-  }
-}
-JS
-
-
-#
-# HTML code
-#
-
-HTML_CODE =<<-HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8" />
-  <title>Ajax upload form</title>
-</head>
-<body>
-<form action="/" method="post" enctype="multipart/form-data" id="form-id">
-  <p><input id="file-id" type="file" name="our-file" /> <input type="button" value="Upload" id="upload-button-id" /></p>
-  <p><label>Some other field: <input name="other-field" type="text" id="other-field-id" /></label></p>
-  <p><input type="submit" value="Save" /></p>
-  <script>
-#{JS_CODE}
-  </script>
-  <p id="upload-status"></p>
-  <p id="progress"></p>
-  <pre id="result"></pre>
-</form>
-</body>
-</html>
-HTML
